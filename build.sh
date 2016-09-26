b797542f8af4658e052869b63a22f86cc6d33b72
Kamas <kamas06@gmail.com>
2016-09-26 14:14:36 +0100

    Added build script

--- build.sh
+++ build.sh
@@ -1,22 +1,4 @@
-React Tutorial 1
-
-
-Run the server from node
-```sh
-$ node server.js
-```
-
-CSS concat
-```
 cat css/*/* css/*.css | sed 's/..\/..\/images/images/g' > bundle.css
-```
-
-Transpile js
-```
 babel --presets react,es2015 js/source -d js/build
-```
-
-Bundle js
-```
 browserify js/build/app.js -o bundle.js
-```
+echo "Done"
