60ab6fb443c6c9a5eee511744fb7395fbaa1a6b7
Kamas <kamas06@gmail.com>
2016-09-28 16:36:02 +0100

    added Suggest

--- js/source/discover.js
+++ js/source/discover.js
@@ -2,6 +2,7 @@
 
 import Logo from './components/Logo';
 import Button from './components/Button';
+import Suggest from './components/Suggest';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
@@ -16,6 +17,10 @@ ReactDOM.render (
 		<h2> Buttons </h2>
 			<div> Button with onClick <Button onClick={() => alert('ouch')}>Click me</Button></div>
 			<div> A link <Button href="http://reactjs.com">Follow me</Button></div>
+		<h2> Suggest </h2>
+		<div>
+			<Suggest options={['enie','minie','miney','mo']} />
+		</div>
 	</div>,
 	document.getElementById('pad')
 );
